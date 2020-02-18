b9ae0e84c1cbc22e519d233837651cd717ae0326
Carlos Teixeira <carlosteixeira.email@gmail.com>
2020-02-18 18:34:12 +0000

    .

--- main.js
+++ main.js
@@ -6,52 +6,57 @@ var port = process.env.PORT || 18733;
 app.set('view engine', 'pug');
 app.use(express.static('static'));
 
-app.get('/home', (req, res) => {
-  res.render('home');
-});
-
-app.get('/genres', (req, res) => {
-  res.render('home');
-});
-
-app.get('/browse', (req, res) => {
-  var musics;
+/*
+app.get('/client', (req, res) => {
   if(req.query.q) {
     request('https://zenmusic-infoapi.herokuapp.com/music/search/' + req.query.q, { json: true }, (err, ress, body) => {
       if (err) {
         console.log(err);
       } else {
         if(req.query.m) {
-          res.render('browse', {
+          res.render('main', {
             musics: body,
             query: req.query.q,
             playermusicid: req.query.m,
             playermusictitle: req.query.t,
             playing: true,
+            page: req.query.page,
+            browse: true
           });
         } else {
-          res.render('browse', {
+          res.render('main', {
             musics: body,
             query: req.query.q,
             playermusicid: req.query.m,
             playermusictitle: req.query.t,
-            playing: false
+            playing: false,
+            page: req.query.page,
+            browse: true
           });
         }
-
       }
+
     });
-
-
-
   } else {
-    res.render('browse');
+    res.render('main');
   }
+
+  });
+*/
+
+app.get('/client', (req, res) => {
+
+          res.render('main', {
+            query: req.query.q,
+            playermusicid: req.query.m,
+            playermusictitle: req.query.t,
+            playing: true,
+            page: req.query.page,
+            browse: true
+          });
+
 });
 
-app.get('/player', (req, res) => {
-  res.render('player');
-});
 
 app.listen(port, () => {
   console.log('mobile app listening on http://localhost:' + port);
